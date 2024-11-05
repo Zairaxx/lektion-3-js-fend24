98ba341bc8deea963ded41bf768500642b4de9fd
Zairaxx <brandontsegai@gmail.com>
2024-11-05 14:48:54 +0100

    alternative loops

--- index.js
+++ index.js
@@ -2,18 +2,51 @@
 
 // for(startvärde, villkor, öka startvärde efter iteration)
 
-let myArr = ["Banan","Körsbär","Jordgubbar","Persika"];
 
 
 let total = 0;
 
-for(let x=0; x < myArr.length; x++){
+for(let x=0; x < 10; x++){
     total +=5;
 }
 
-console.log(total);
+// console.log(total)
 
 //While-loop
+let y = 0;
+
+while(y < 10){
+    
+    console.log(y);
+    y++;
+}
 
 
-//forEach-metod
+//forEach-metod - Detta är en array-metod och kan ENDAST användas för arrayer
+let myArr = ["Banan","Körsbär","Jordgubbar","Persika", "Äpple"];
+// for(let x=0; x < myArr.length; x++){
+//     console.log(myArr[x]);
+// }
+
+let berries = [];
+let fruits = [];
+
+//Non-mutative
+myArr.forEach((item) => {
+    if(item === "Körsbär" || item === "Jordgubbar"){
+        berries.push(item);
+    } else {
+        fruits.push(item)
+    }
+})
+
+console.log(myArr);
+console.log("Berries: ", berries);
+console.log("Fruits: ", fruits);
+
+//Vanlig funktion vs Array function
+// function checkItem(item){
+//     console.log(item)
+// }
+
+// let checkItem = (item) => { console.log(item)}
